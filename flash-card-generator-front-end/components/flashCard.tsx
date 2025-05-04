819d3367c5b9f715a99dd17b3336a5ad28a48e9b
DESKTOP-K4DMIG9\iamse <seth.rhodes21@gmail.com>
2025-05-04 21:47:26 +0100

    basic test deck renders

---
+++ flash-card-generator-front-end/components/flashCard.tsx
@@ -0,0 +1,28 @@
+'use client'
+import { IFlashCard } from "@/apiClient";
+
+type CardProps = {
+  card: IFlashCard;
+};
+
+export default function Card({card} : CardProps) {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center w-full justify-between p-4 bg-white shadow-md rounded-lg text-black">
+        <div className="flex flex-row items-center">
+          <p>{card.level}</p>
+          <p>{card.frontView.language.textCode}</p>
+          <p>{card.frontView.region}</p>
+        </div>
+        <p>{card.frontView.text}</p>
+      </div>
+      <div className="flex flex-col items-center w-full justify-between p-4 bg-white shadow-md rounded-lg text-black">
+        <div className="flex flex-row items-center">
+          <p>{card.backView.language.textCode}</p>
+          <p>{card.backView.region}</p>
+        </div>
+        <p>{card.backView.text}</p>
+    </div>
+    </div>
+  );
+}
---
+++ flash-card-generator-front-end/scripts/create-local-server.js
@@ -0,0 +1,31 @@
+import { createServer as createHttpsServer } from 'https';
+import next from 'next';
+import fs from 'fs';
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const PORT = process.env.PORT || 3000;
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+
+app
+  .prepare()
+  .then(() => {
+    const server = createHttpsServer(
+      {
+        key: fs.readFileSync('./localhost-key.pem'), // Path to your private key
+        cert: fs.readFileSync('./localhost.pem'),    // Path to your certificate
+      },
+      (req, res) => handle(req, res)
+    );
+
+    return server.listen(PORT, (err) => {
+      if (err) throw err;
+
+      console.log('> Ready on https://localhost:3000');
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
